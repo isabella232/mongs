d8988ae6a5ea4234abbb1f55e63d8115288417e8
Jason R. Coombs <jaraco@jaraco.com>
2012-07-16 16:43:50 -0400

    Added support for translating the config to a format that aspen might expect

--- aspen-vr.py
+++ aspen-vr.py
@@ -6,6 +6,16 @@ import os
 import yaml
 import ConfigParser
 
+def translate(value):
+	"""
+	Take a YAML value and make it a suitable string for ConfigParser.
+	"""
+	if isinstance(value, (list, tuple)):
+		return '\n    '.join(value)
+	if isinstance(value, dict):
+		raise ValueError("Can't translate a dict to ConfigParser format")
+	return value
+
 def convert_config():
 	with open(os.environ['APP_SETTINGS_YAML']) as f:
 		in_config = yaml.load(f)
@@ -14,6 +24,7 @@ def convert_config():
 		out_config.add_section(section)
 		for field in in_config[section]:
 			value = in_config[section][field]
+			value = translate(value)
 			out_config.set(section, field, value)
 	os.path.isdir('.aspen') or os.makedirs('.aspen')
 	with open('.aspen/aspen.conf', 'wb') as f:
